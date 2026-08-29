d0f98ca7d3de842ee64438c77eb3cd7ab097fb99
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-29 17:21:31 +0100

    Install Docker settings dashboard patch at startup

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from nekosuneai.avatar_http_patch import install_avatar_http_patch
 from nekosuneai.mcp_oauth_recovery import install_mcp_oauth_recovery
+from nekosuneai.settings_dashboard_patch import install_settings_dashboard_patch
 
 install_avatar_http_patch()
 install_mcp_oauth_recovery()
+install_settings_dashboard_patch()
 
 from nekosuneai.launcher import main
 
