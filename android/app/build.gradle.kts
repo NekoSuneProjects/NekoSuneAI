3ade6cd965731d0343239b81e513fe2c10fe384d
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-29 16:48:54 +0100

    Restore compile SDK 36 required by CameraX 1.6.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "co.uk.nekosuneprojects.nekosuneai"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "co.uk.nekosuneprojects.nekosuneai"
