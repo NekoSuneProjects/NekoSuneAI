89d661079260d7bd3775931d14e8eb43dc197817
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-29 15:01:53 +0100

    Require Android 14+ for companion APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "co.uk.nekosuneprojects.nekosuneai"
-        minSdk = 26
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
