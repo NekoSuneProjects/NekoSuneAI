3860aea122fa237cd0e5db615f0029dd1286ae98
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-08-29 18:04:08 +0100

    Enable periodic yt-dlp nightly refresh

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from nekosuneai.settings_dashboard_patch import install_settings_dashboard_patch
 from nekosuneai.settings_backend_patch import install_settings_backend_patch
 from nekosuneai.settings_dashboard_compat import install_settings_dashboard_compat
 from nekosuneai.music_dashboard_patch import install_music_dashboard_patch
+from nekosuneai.ytdlp_nightly_patch import install_ytdlp_nightly_patch
 
 install_avatar_http_patch()
 install_mcp_oauth_recovery()
@@ -11,6 +12,7 @@ install_settings_dashboard_patch()
 install_settings_backend_patch()
 install_settings_dashboard_compat()
 install_music_dashboard_patch()
+install_ytdlp_nightly_patch()
 
 from nekosuneai.launcher import main
 
